8c5935b29010570a5b4744d864b3f4b6f831c9cd
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-05 02:00:54 +0200

    Added a timestamp.

--- EmotionalSongsClient/src/main/java/emotionalsongs/PingClientClientImpl.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/PingClientClientImpl.java
@@ -1,5 +1,6 @@
 package emotionalsongs;
 
+import java.io.Serial;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Calendar;
@@ -8,6 +9,7 @@ import java.util.GregorianCalendar;
 
 class PingClientClientImpl extends UnicastRemoteObject implements PingClient {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private static Calendar calendar;
@@ -19,8 +21,8 @@ class PingClientClientImpl extends UnicastRemoteObject implements PingClient {
     }
 
     public void pingClient() throws RemoteException{
-
-        System.out.println("["+calendar.get(Calendar.HOUR_OF_DAY)+":"+ calendar.get(Calendar.MINUTE)  + "] I've been pinged by the server!");
+        calendar.setTime(new Date());
+        System.out.println("["+ String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE) )+ "] "+ "I've been pinged by the server!");
     }
 
 
