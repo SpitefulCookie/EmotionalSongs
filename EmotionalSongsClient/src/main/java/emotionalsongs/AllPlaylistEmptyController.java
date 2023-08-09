be085a337bfebf7af27a51062279a853f95dbffb
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-09 05:11:14 +0200

    Modificato con caricamento scena all'interno di GUIUtilities

--- EmotionalSongsClient/src/main/java/emotionalsongs/AllPlaylistEmptyController.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/AllPlaylistEmptyController.java
@@ -18,17 +18,13 @@ public class AllPlaylistEmptyController {
     @FXML
     protected void handleCreatePlaylistButtonAction(){
         // TODO aprire il pane createPlaylist.fxml
-        try {
-            Stage createPlaylistStage = new Stage();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createPlaylist.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            createPlaylistStage.setScene(scene);
-            createPlaylistStage.initStyle(StageStyle.UNDECORATED);
-            createPlaylistStage.setResizable(false);
-            createPlaylistStage.initModality(Modality.APPLICATION_MODAL);
-            createPlaylistStage.show();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        Stage createPlaylistStage = new Stage();
+
+        createPlaylistStage.setScene(GUIUtilities.getInstance().getScene("createPlaylist.fxml"));
+        createPlaylistStage.initStyle(StageStyle.UNDECORATED);
+        createPlaylistStage.setResizable(false);
+        createPlaylistStage.initModality(Modality.APPLICATION_MODAL);
+        createPlaylistStage.show();
+
     }
 }
