7574837d8a3f7fade1adab279ea264d72a4332cf
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-09 05:22:02 +0200

    Minor changes

--- EmotionalSongsClient/src/main/java/emotionalsongs/AuthManager.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/AuthManager.java
@@ -12,7 +12,7 @@ import java.security.PublicKey;
 public interface AuthManager extends Remote {
 
     boolean usernameExists(String username) throws RemoteException;
-    void registrazione(byte[] user) throws RemoteException; // temporarily set to String instead of Utente
+    void registrazione(byte[] user) throws RemoteException;
     boolean userLogin(String username, byte[] pwd) throws RemoteException;
     PublicKey getPublicKey() throws RemoteException;
     void registerClient(PingClient client) throws RemoteException;
