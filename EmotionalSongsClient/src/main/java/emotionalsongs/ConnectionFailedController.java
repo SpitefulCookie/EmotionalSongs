4d7a366695cef15f349c8f7dd6dac9d39c4fb70b
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-09 05:14:35 +0200

    Effettuata inizializzazione della connessione del server all'interno della classe EmotionalSongsClient

--- EmotionalSongsClient/src/main/java/emotionalsongs/ConnectionFailedController.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/ConnectionFailedController.java
@@ -1,19 +1,10 @@
 package emotionalsongs;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-import java.io.IOException;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 
 public class ConnectionFailedController {
 
@@ -27,31 +18,10 @@ public class ConnectionFailedController {
      */
     @FXML
     public void handleTryAgainButtonAction(){
-        // TODO riprovare connessione con server
-        try{
-            // try again to connect to the server
-            Registry reg = LocateRegistry.getRegistry(EmotionalSongsClient.getServerAddress(), EmotionalSongsClient.getPort());
-            EmotionalSongsClient.auth = (AuthManager) reg.lookup("AuthManager");
-            // close the connection failed stage
-            closeConnectionFailedStage(tryAgainButton);
-        }catch (RemoteException | NotBoundException e){
-            // close the connection failed stage
-            closeConnectionFailedStage(tryAgainButton);
-            // display the connection failed stage
-            try{
-                Stage connectionFailedStage = new Stage();
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("connectionFailed.fxml"));
-                Scene connectionFailedScene = new Scene(fxmlLoader.load());
-                connectionFailedStage.setScene(connectionFailedScene);
-                connectionFailedStage.initStyle(StageStyle.UNDECORATED);
-                connectionFailedStage.initModality(Modality.APPLICATION_MODAL);
-                connectionFailedStage.setResizable(false);
-                connectionFailedStage.show();
-            }catch(IOException e1){
-                e1.printStackTrace();
-            }
-        }
+        closeConnectionFailedStage(tryAgainButton);
+        EmotionalSongsClient.initializeServerConnection();
     }
+
 
     /**
      * TODO document
@@ -61,7 +31,7 @@ public class ConnectionFailedController {
         // close the connection failed stage
         closeConnectionFailedStage(exitButton);
         // close the main stage
-        //EmotionalSongsClient.getStage().close(); // TODO togliere dal commento
+        //EmotionalSongsClient.getStage().close(); // TODO togliere
     }
 
     /**
