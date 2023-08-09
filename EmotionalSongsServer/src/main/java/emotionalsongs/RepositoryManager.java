502639f215a283e51153cfa08500739c7d21822e
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-09 18:06:16 +0200

    Latest version

--- EmotionalSongsServer/src/main/java/emotionalsongs/RepositoryManager.java
+++ EmotionalSongsServer/src/main/java/emotionalsongs/RepositoryManager.java
@@ -10,6 +10,7 @@ public interface RepositoryManager extends Remote {
 
     // TODO Ho optato per una hashSet invece che una ArrayList per migliori prestazioni (la H.S non garantisce l'ordine degli elementi però non penso che ce ne freghi.
     HashSet<Canzone> ricercaCanzone(String titolo) throws RemoteException; // todo rename method
+    HashSet<Canzone> ricercaCanzone(String autore, String anno) throws RemoteException;
     HashSet<String> getUserPlaylists(String user) throws RemoteException;
     HashSet<Canzone> getSongsInPlaylist(String playlistName) throws RemoteException;
     void registerPlaylist (String username, String playlistName, String loggedUser) throws RemoteException;
