8c579667426b4fc7f5a4e467dbf2c10801c6918c
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-09 05:12:38 +0200

    Aggiunta metodo per effettuare la criptazione con RSA

--- EmotionalSongsClient/src/main/java/emotionalsongs/AuthManager.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/AuthManager.java
@@ -1,5 +1,7 @@
 package emotionalsongs;
 
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.security.PublicKey;
@@ -9,9 +11,20 @@ import java.security.PublicKey;
  */
 public interface AuthManager extends Remote {
 
-    public boolean usernameExists(String username) throws RemoteException;
-    public void registrazione(byte[] user) throws RemoteException; // temporarily set to String instead of Utente
-    public boolean userLogin(String username, byte[] pwd) throws RemoteException;
-    public PublicKey getPublicKey() throws RemoteException;
-    public void registerClient(PingClient client) throws RemoteException;
+    boolean usernameExists(String username) throws RemoteException;
+    void registrazione(byte[] user) throws RemoteException; // temporarily set to String instead of Utente
+    boolean userLogin(String username, byte[] pwd) throws RemoteException;
+    PublicKey getPublicKey() throws RemoteException;
+    void registerClient(PingClient client) throws RemoteException;
+
+    static byte[] RSA_Encrypt(String data){
+        try {
+            PublicKey pk = EmotionalSongsClient.auth.getPublicKey();
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, pk);
+            return encryptCipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e){
+            return null;
+        }
+    }
 }
