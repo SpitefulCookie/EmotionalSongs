c6a139b36c4d90d1d32dd50ee4ed54aab44171ce
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-10 18:38:07 +0200

    Added support to load all application images at launch

--- EmotionalSongsServer/src/main/java/emotionalsongs/GUIUtilities.java
+++ EmotionalSongsServer/src/main/java/emotionalsongs/GUIUtilities.java
@@ -4,10 +4,49 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class GUIUtilities {
+
+    private final HashMap<String, Image> images;
+    private static GUIUtilities guiUtilities;
+
+    GUIUtilities(){
+
+        this.images = new HashMap<>();
+
+        try{
+
+            images.put("eye", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/view.png")));
+            images.put("eyeCrossed", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/hide.png")));
+            images.put("blueFire", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/blueFire.png")));
+            images.put("gear", new Image( new FileInputStream("EmotionalSongsServer/src/main/resources/Images/gear2.png")));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // pattern singleton
+    public static GUIUtilities getInstance(){
+        if(guiUtilities == null){guiUtilities = new GUIUtilities();}
+        return guiUtilities;
+    }
+
+    protected Image getImage(String key){
+
+        // se la hash map contiene la key passata come parametro allora restituisci l'immagine associata alla chiave
+        if(images.containsKey(key)) { return images.get(key);}
+        else{ return null;}// altrimenti restituisci null
+    }
+
 
     /**
      * TODO document
