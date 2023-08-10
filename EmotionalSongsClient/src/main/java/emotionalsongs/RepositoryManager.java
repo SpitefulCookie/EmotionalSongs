76b984059f5c1156b79911885859e705e039a229
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-10 14:57:54 +0200

    Fixed error in method prototypes registerPlaylist & addSongToPlaylist.

--- EmotionalSongsClient/src/main/java/emotionalsongs/RepositoryManager.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/RepositoryManager.java
@@ -8,16 +8,13 @@ import java.util.HashSet;
 
 public interface RepositoryManager extends Remote {
 
-    // TODO Ho optato per una hashSet invece che una ArrayList per migliori prestazioni (la H.S non garantisce l'ordine degli elementi però non penso che ce ne freghi.
     HashSet<Canzone> ricercaCanzone(String titolo) throws RemoteException; // todo rename method
     HashSet<Canzone> ricercaCanzone(String autore, String anno) throws RemoteException;
     HashSet<String> getUserPlaylists(String user) throws RemoteException;
     HashSet<Canzone> getSongsInPlaylist(String playlistName) throws RemoteException;
-    void registerPlaylist (String username, String playlistName, String loggedUser) throws RemoteException;
-    void addSongToPlaylist( String username, String nomePlaylist) throws RemoteException;
+    void registerPlaylist (String username, String playlistName) throws RemoteException;
+    void addSongToPlaylist( String username, String nomePlaylist, String songUUID) throws RemoteException;
     void registerEmotions(ArrayList<Emozione> emozioniProvate, String songUUID, String userId) throws RemoteException;
     ArrayList<Emozione> getSongEmotions(String songUUID, String userid) throws RemoteException;
-
-    //int[] getPlaylistSongs(String playlistName) throws RemoteException; // TODO (int[]? che canna avevamo fumato??)
 
 }
