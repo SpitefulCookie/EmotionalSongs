257d69cc4497be46fb46c78e6347301867fa7b33
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-11 16:06:23 +0200

    Added Javadoc documentation

--- EmotionalSongsServer/src/main/java/emotionalsongs/PingClient.java
+++ EmotionalSongsServer/src/main/java/emotionalsongs/PingClient.java
@@ -3,6 +3,22 @@ package emotionalsongs;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ *
+ * This interface defines a method to be implemented by remote ping clients.
+ * The purpose of the method is to allow the server to ping the client to check their responsiveness.
+ *
+ * @author <a href="https://github.com/SpitefulCookie"> Della Chiesa Mattia</a>
+ */
 public interface PingClient extends Remote {
-    public void pingClient() throws RemoteException;
+
+    /**
+     * Pings the remote client to check its responsiveness.<br><br>
+     *
+     * This method is intended to be invoked by the server to determine if the client is responsive.
+     * The responsiveness of the client is checked through {@code RemoteException}.<br><br>
+     *
+     * @throws RemoteException If a communication-related exception occurs during the ping process.
+     */
+    void pingClient() throws RemoteException;
 }
