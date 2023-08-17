0044de2eb66c6a6b6d30d0da0495f61e7686ead9
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-17 18:42:35 +0200

    Added function to format text (used serverside).

--- EmotionalSongsServer/src/main/java/emotionalsongs/GUIUtilities.java
+++ EmotionalSongsServer/src/main/java/emotionalsongs/GUIUtilities.java
@@ -1,16 +1,18 @@
 package emotionalsongs;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GUIUtilities {
 
@@ -26,10 +28,10 @@ public class GUIUtilities {
             images.put("eye", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/view.png")));
             images.put("eyeCrossed", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/hide.png")));
             images.put("blueFire", new Image(new FileInputStream("EmotionalSongsServer/src/main/resources/Images/blueFire.png")));
-            images.put("gear", new Image( new FileInputStream("EmotionalSongsServer/src/main/resources/Images/gear2.png")));
+            images.put("settings", new Image( new FileInputStream("EmotionalSongsServer/src/main/resources/Images/gear.png")));
 
         }catch (IOException e){
-            e.printStackTrace();
+            System.err.println("IOException thrown while attempting to load FXML scene files. \nReason: " + e.getMessage());
         }
 
     }
@@ -38,6 +40,72 @@ public class GUIUtilities {
     public static GUIUtilities getInstance(){
         if(guiUtilities == null){guiUtilities = new GUIUtilities();}
         return guiUtilities;
+    }
+
+    public static ArrayList<Text> formatText(String input) {
+
+        ArrayList<Text> formattedTextList = new ArrayList<>();
+
+        boolean textContainedBold = false;
+
+        // Define regex patterns to match bold and italic formatting
+        Pattern boldPattern = Pattern.compile("\\*\\*(.*?)\\*\\*");
+        Pattern italicPattern = Pattern.compile("__([^_]+)__");
+
+        // Get the default font and create fonts for bold and italic styles
+        Font defaultFont = Font.getDefault();
+        Font boldFont = Font.font(defaultFont.getFamily(), FontWeight.BOLD, defaultFont.getSize());
+        Font italicFont = Font.font(defaultFont.getFamily(), FontPosture.ITALIC, defaultFont.getSize());
+
+        // Create matchers to find bold and italic patterns in the input text
+        Matcher boldMatcher = boldPattern.matcher(input);
+        Matcher italicMatcher = italicPattern.matcher(input);
+
+        // Initialize the current index for tracking progress through the input text
+        int currentIndex = 0;
+
+        // Process bold patterns in the input text
+        while (boldMatcher.find()) {
+            // Add normal text before the bold pattern (if any)
+            if (boldMatcher.start() > currentIndex) {
+                formattedTextList.add(new Text(input.substring(currentIndex, boldMatcher.start())));
+            }
+
+            // Create a Text node for the bold pattern and set its font to bold
+            Text boldText = new Text(boldMatcher.group(1));
+            boldText.setFont(boldFont);
+            formattedTextList.add(boldText);
+            textContainedBold = true;
+
+            // Update the current index
+            currentIndex = boldMatcher.end();
+        }
+
+        // Process italic patterns in the input text
+        while (italicMatcher.find()) {
+
+
+            // Add normal text before the italic pattern (if any)
+            if (italicMatcher.start() > currentIndex) {
+                formattedTextList.add(new Text(input.substring(currentIndex, italicMatcher.start())));
+            }
+
+            // Create a Text node for the italic pattern and set its font to italic
+            Text italicText = new Text(italicMatcher.group(1));
+            italicText.setFont(italicFont);
+            formattedTextList.add(italicText);
+
+            // Update the current index
+            currentIndex = italicMatcher.end();
+        }
+
+        // Add any remaining normal text at the end of the input
+        if (currentIndex < input.length()) {
+            formattedTextList.add(new Text(input.substring(currentIndex)));
+        }
+
+        // Return the list of formatted Text nodes
+        return formattedTextList;
     }
 
     protected Image getImage(String key){
@@ -55,19 +123,14 @@ public class GUIUtilities {
      */
     protected static void forceTextInput(final TextField tf, final int maxLen) {
 
-        tf.textProperty().addListener(new ChangeListener<String>() {
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
 
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-
-                if (newValue.matches(".*\\d.*")) {
-                    tf.setText(newValue.replaceAll("\\d", ""));
-                }else if (tf.getText().length() > maxLen) {
-                    String s = tf.getText().substring(0, maxLen);
-                    tf.setText(s);
-                }
+            if (newValue.matches(".*\\d.*")) {
+                tf.setText(newValue.replaceAll("\\d", ""));
+            }else if (tf.getText().length() > maxLen) {
+                String s = tf.getText().substring(0, maxLen);
+                tf.setText(s);
             }
-
         });
     }
 
@@ -77,13 +140,9 @@ public class GUIUtilities {
      */
     protected static void forceNumericInput(final TextField tf) {
 
-        tf.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue,
-                                String newValue) {
-                if (!newValue.matches("\\d*")) {
-                    tf.setText(newValue.replaceAll("[^\\d]", ""));
-                }
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tf.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
 
@@ -96,16 +155,12 @@ public class GUIUtilities {
      */
     protected static void forceNumericInput(final TextField tf, final int maxLen) {
 
-        tf.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue,
-                                String newValue) {
-                if (!newValue.matches("\\d*")) {
-                    tf.setText(newValue.replaceAll("[^\\d]", ""));
-                }else if (tf.getText().length() > maxLen) {
-                    String s = tf.getText().substring(0, maxLen);
-                    tf.setText(s);
-                }
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tf.setText(newValue.replaceAll("[^\\d]", ""));
+            }else if (tf.getText().length() > maxLen) {
+                String s = tf.getText().substring(0, maxLen);
+                tf.setText(s);
             }
         });
 
@@ -117,13 +172,10 @@ public class GUIUtilities {
      * @param maxLength
      */
     protected static void addTextLimiter(final TextField tf, final int maxLength) {
-        tf.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
-                if (tf.getText().length() > maxLength) {
-                    String s = tf.getText().substring(0, maxLength);
-                    tf.setText(s);
-                }
+        tf.textProperty().addListener((ov, oldValue, newValue) -> {
+            if (tf.getText().length() > maxLength) {
+                String s = tf.getText().substring(0, maxLength);
+                tf.setText(s);
             }
         });
     }
@@ -134,13 +186,9 @@ public class GUIUtilities {
      */
     protected static void forceTextInput(final TextField tf) {
 
-        tf.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue,
-                                String newValue) {
-                if (newValue.matches("\\d*")) {
-                    tf.setText(newValue.replaceAll("[\\d]", ""));
-                }
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.matches("\\d*")) {
+                tf.setText(newValue.replaceAll("[\\d]", ""));
             }
         });
 
