0c04aeac412272ee624225780bdc3776727d9889
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-17 23:34:35 +0200

    Added project requirement for java.sql (SQLExceptions)

--- EmotionalSongsClient/src/main/java/module-info.java
+++ EmotionalSongsClient/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module emotionalsongs.emotionalsongsclient {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.rmi;
+    requires java.sql;
 
 
     opens emotionalsongs to javafx.fxml;
