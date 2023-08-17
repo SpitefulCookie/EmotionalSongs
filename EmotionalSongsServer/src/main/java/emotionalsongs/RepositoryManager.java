f123839f03c0476e59db1895f9bbda474b687f4b
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-17 18:26:18 +0200

    Added getSongAverageEmotions and SQLExceptions thrown within methods registerPlaylist, addSongToPlaylist, registerEmotions.

--- EmotionalSongsServer/src/main/java/emotionalsongs/RepositoryManager.java
+++ EmotionalSongsServer/src/main/java/emotionalsongs/RepositoryManager.java
@@ -2,6 +2,7 @@ package emotionalsongs;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -12,10 +13,10 @@ public interface RepositoryManager extends Remote {
     HashSet<Canzone> ricercaCanzone(String autore, String anno) throws RemoteException;
     HashSet<String> getUserPlaylists(String user) throws RemoteException;
     HashSet<Canzone> getSongsInPlaylist(String playlistName, String username) throws RemoteException;
-    void registerPlaylist (String playlistName, String username) throws RemoteException;
-    void addSongToPlaylist(String nomePlaylist, String userID, String songUUID) throws RemoteException;
-    void registerEmotions(ArrayList<Emozione> emozioniProvate, String songUUID, String userId) throws RemoteException;
+    void registerPlaylist (String playlistName, String username) throws RemoteException, SQLException;
+    void addSongToPlaylist(String nomePlaylist, String userID, String songUUID) throws RemoteException, SQLException;
+    void registerEmotions(ArrayList<Emozione> emozioniProvate, String songUUID, String userId) throws RemoteException, SQLException;
     ArrayList<Emozione> getSongEmotions(String songUUID, String userid) throws RemoteException;
-
+    double[] getSongAverageEmotions(String songUUID) throws RemoteException;
 
 }
