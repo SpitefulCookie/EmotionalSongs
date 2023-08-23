ede03ae7982d820b0b20fdb6cba0273c83c6f2a1
Clookie <69983139+SpitefulCookie@users.noreply.github.com>
2023-08-24 01:29:42 +0200

    Added JavaDoc.

--- EmotionalSongsClient/src/main/java/emotionalsongs/EmotionalSongsClient.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/EmotionalSongsClient.java
@@ -1,6 +1,6 @@
 package emotionalsongs;
 
-/* TODO Add this to all java classes
+/*
  * Progetto svolto da:
  * Corallo Samuele 749719, Ateneo di Varese
  * Della Chiesa Mattia 749904, Ateneo di Varese
@@ -13,7 +13,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.IOException;
 import java.rmi.NoSuchObjectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -21,12 +20,22 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-/**
- * TODO Document
- */
 /*
 TODO: aggiungere a tutte le classi i metodi richiesti dal progetto
 */
+
+/**
+ * The main class for the EmotionalSongs client application. Manages the user interface, server connection,
+ * and various utility methods related to client functionality.
+ * <p>
+ * This class extends {@link Application} and serves as the entry point for the client application. It handles
+ * initializing the graphical user interface, managing the server connection, and providing various utility methods
+ * for client operations.
+ * <p>
+ * The client application uses JavaFX for its graphical interface and relies on RMI to communicate with the server
+ * for authentication and data retrieval.
+ *
+ */
 public class EmotionalSongsClient extends Application {
 
     private static Stage esStage;
@@ -39,9 +48,14 @@ public class EmotionalSongsClient extends Application {
     protected static boolean isConnectionInitialized = false;
 
     /**
-     * TODO Document
-     * @param stage
-     * @throws IOException
+     * Starts the client application and initializes its primary stage.<br><br>
+     *
+     * <p>This method is the entry point for the client application.
+     * It invokes the methods {@link GUIUtilities#initNode()} and {@link GUIUtilities#initScenes()} from the class
+     * {@code GUIUtilities} which are responsible for loading and initializing the majority of the application's graphical interfaces;
+     * It then initializes and displays the login screen.
+     *
+     * @param stage The primary {@link Stage} of the client application.
      */
     @Override
     public void start(Stage stage){
@@ -65,6 +79,13 @@ public class EmotionalSongsClient extends Application {
 
     }
 
+    /**
+     * Initializes the server connection by looking up remote objects from the RMI registry.<br><br>
+     * This method is responsible for initializing the connection with the server by looking up the remote objects from
+     * the RMI registry; If the connection attempt fails, this method will display a connection warning.
+     *
+     * @param suppressConnectionWarning If true, suppresses the connection warning dialog.
+     */
     protected static void initializeServerConnection(boolean suppressConnectionWarning){
 
         isConnectionInitialized = false;
@@ -92,19 +113,21 @@ public class EmotionalSongsClient extends Application {
     }
 
     /**
-     * TODO Document
-     * @return
+     * Retrieves the primary stage of the application.
+     *
+     * @return The primary {@link Stage} of the application.
      */
     protected static Stage getStage(){
         return esStage;
     }
 
     /**
-     * TODO Document
-     * @param scene
-     * @param width
-     * @param height
-     * @param isWindowResizable
+     * Sets the stage with a new scene and specified dimensions.
+     *
+     * @param scene The new {@link Scene} to be set.
+     * @param width The width of the stage.
+     * @param height The height of the stage.
+     * @param isWindowResizable Determines if the window is resizable.
      */
     protected static void setStage(Scene scene, int width, int height, boolean isWindowResizable){
         esStage.close();
@@ -116,11 +139,15 @@ public class EmotionalSongsClient extends Application {
     }
 
     /**
-     * TODO Document?
-     * @param args
+     * Entry point for launching the client application.
+     *
+     * @param args The command-line arguments. (Unused)
      */
-    public static void main(String[] args) {new EmotionalSongsClient().launch();}
+    public static void main(String[] args) {launch();}
 
+    /**
+     * Disconnects the client from the server, unregistering it and unexporting the remote object.
+     */
     public static void disconnectClient(){
         try {
             if(auth!=null)
@@ -134,6 +161,10 @@ public class EmotionalSongsClient extends Application {
         }
     }
 
+    /**
+     * Registers the client with the server.<br>
+     * The {@link PingableClientClientImpl} reference provided will be used as a mean to remotely ping the client from the server.
+     */
     public static void registerClient(){
         try {
             if(auth!= null)
