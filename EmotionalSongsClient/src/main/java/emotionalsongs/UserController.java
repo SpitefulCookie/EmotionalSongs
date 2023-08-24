3bc40b124ae6ee9ae65bcaea871fe0a678ac99e0
samuk52 <samuele.corallo10@gmail.com>
2023-08-24 23:12:24 +0200

    user pane completed

--- EmotionalSongsClient/src/main/java/emotionalsongs/UserController.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/UserController.java
@@ -3,37 +3,63 @@ package emotionalsongs;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
-public class UserController implements Initializable {
+public class UserController {
 
-    @FXML private Label nameLabel;
-    @FXML private Label surnameLabel;
+    @FXML private Label usernameLabel;
+    @FXML private Label nameAndLastNameLabel;
+    @FXML private Label codiceFiscaleLabel;
     @FXML private Label emailLabel;
     @FXML private Label addressLabel;
 
+    /**
+     * TODO document
+     * @param user
+     */
+    protected void setUser(String user){
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // retrive the user data
+        String[] userData = retrieveUserData(user).split(",");
 
-        // TODO interrogare il db per farsi restituire le informazioni dell'utente
-
-        // TODO settare le varie label
+        // set the label
+        usernameLabel.setText(user);
+        nameAndLastNameLabel.setText(" " + userData[0]);
+        codiceFiscaleLabel.setText(userData[1]);
+        emailLabel.setText(userData[2]);
+        addressLabel.setText(userData[3] + " n." + userData[4] + "   " + userData[5] + " -" + userData[7] + " (" + userData[6].toUpperCase() + " )");
 
         // DEBUG TODO remove
-        System.out.println("initialize user effettuato");
+        System.out.println("Dati dell'utente: " + EmotionalSongsClientController.getUsername() + " -> " + Arrays.toString(userData));
 
     }
 
-    public static String retrieveUserData() { // TODO static aggiunto solo per effettuare dei test, vedi tu la segnatura del metodo
+    /**
+     * TODO document
+     * @return
+     */
+    protected String retrieveUserData(String user) {
         try {
-            String data= AuthManager.decryptRSA(EmotionalSongsClient.auth.getUserData("test", EmotionalSongsClient.ping.getPublicKey()),EmotionalSongsClient.ping.getPrivateKey()); // TODO rimpiazzare 'test' con l'userId dell'utente loggato
+            String data= AuthManager.decryptRSA(EmotionalSongsClient.auth.getUserData(user, EmotionalSongsClient.ping.getPublicKey()),EmotionalSongsClient.ping.getPrivateKey()); // TODO rimpiazzare 'test' con l'userId dell'utente loggato
             return data.substring(1,data.length()-1);
         } catch (RemoteException e) {
-            e.printStackTrace(); // TODO decidi tu come gestire l'eccezione
+
+            Stage connectionFailedStage = new Stage();
+
+            connectionFailedStage.setScene(GUIUtilities.getInstance().getScene("connectionFailed.fxml"));
+            connectionFailedStage.initStyle(StageStyle.UNDECORATED);
+            connectionFailedStage.initModality(Modality.APPLICATION_MODAL);
+            connectionFailedStage.setResizable(false);
+            connectionFailedStage.show();
+
             return "";
         }
     }
