c608d852468eb0ea488d45f2ef7d0f06a87e30dd
samuk52 <samuele.corallo10@gmail.com>
2023-08-27 14:51:25 +0200

    added javaDoc

--- EmotionalSongsClient/src/main/java/emotionalsongs/AllPlaylistEmptyController.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/AllPlaylistEmptyController.java
@@ -1,5 +1,13 @@
 package emotionalsongs;
 
+/*
+ * Progetto svolto da:
+ *
+ * Corallo Samuele 749719, Ateneo di Varese
+ * Della Chiesa Mattia 749904, Ateneo di Varese
+ *
+ */
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,15 +18,29 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
+/**
+ * The controller class to display the {@link javafx.scene.Node} pane which informs the user
+ * that it has no playlists created.
+ *
+ * @author <a href="https://github.com/samuk52">Corallo Samuele</a>
+ */
 public class AllPlaylistEmptyController {
 
     @FXML
     private Button createPlaylistBtn;
     private boolean fromAddToPlaylist;
 
+    /**
+     * Method that handles the behaviour of the {@link AllPlaylistEmptyController#createPlaylistBtn}.
+     * <p>
+     *     When {@link AllPlaylistEmptyController#createPlaylistBtn} is clicked, the {@link Stage} (window) controlled
+     *     by the class {@link CreatePlaylistController} opens, allowing the creation of a new playlist.
+     * </p>
+     */
     @FXML
     protected void handleCreatePlaylistButtonAction(){
 
+        // open the createPlaylist stage.
         try{
             Stage createPlaylistStage = new Stage();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createPlaylist.fxml"));
@@ -39,12 +61,12 @@ public class AllPlaylistEmptyController {
     }
 
     /**
-     * TODO document
-     * @param fromAddToPlaylist
+     * Method that sets {@link AllPlaylistEmptyController#fromAddToPlaylist}.
+     *
+     * @param fromAddToPlaylist represents the {@code boolean} variable with which the {@link AllPlaylistEmptyController#fromAddToPlaylist} will be set.
      */
     public void setFromAddToPlaylist(boolean fromAddToPlaylist){
         this.fromAddToPlaylist = fromAddToPlaylist;
     }
-
 
 }
