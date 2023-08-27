fe6b0e652c194d680faa8a29c310e8daf5473218
samuk52 <samuele.corallo10@gmail.com>
2023-08-27 15:26:40 +0200

    added javaDoc

--- EmotionalSongsClient/src/main/java/emotionalsongs/ConnectionFailedController.java
+++ EmotionalSongsClient/src/main/java/emotionalsongs/ConnectionFailedController.java
@@ -1,18 +1,37 @@
 package emotionalsongs;
 
+/*
+ * Progetto svolto da:
+ *
+ * Corallo Samuele 749719, Ateneo di Varese
+ * Della Chiesa Mattia 749904, Ateneo di Varese
+ *
+ */
+
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 
+/**
+ * The controller class that handles the display of the {@link Stage} (window) informing the user that the
+ * connection with the server has failed, this window is displayed when a {@link java.rmi.RemoteException} is generated.
+ *
+ * @author <a href="https://github.com/samuk52">Corallo Samuele</a>
+ * @author <a href="https://github.com/SpitefulCookie"> Della Chiesa Mattia</a>
+ */
 public class ConnectionFailedController {
 
     @FXML private Button tryAgainButton;
     @FXML private Button exitButton;
 
     /**
-     * TODO document
+     * Method that handles the behaviour of the {@link ConnectionFailedController#tryAgainButton}.
+     * <p>
+     *     When the {@link ConnectionFailedController#tryAgainButton} is clicked, an attempt is made to
+     *     reconnect with the server.
+     * </p>
      */
     @FXML
     public void handleTryAgainButtonAction(){
@@ -22,7 +41,10 @@ public class ConnectionFailedController {
 
 
     /**
-     * TODO document
+     * Method that handles the behaviour of the {@link ConnectionFailedController#exitButton}.
+     * <p>
+     *     When the {@link ConnectionFailedController#exitButton} is clicked, the method closes the window containing the button.
+     * </p>
      */
     @FXML
     public void handleExitButtonAction(){
